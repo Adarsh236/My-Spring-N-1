7511b9ff1cbf7ee4fbb9a3b9779a918f3be88836
adarshverma236 <adarshverma11236@gmail.com>
2020-02-08 05:48:35 +0200

    Primary with Qualifier

--- Spring-2- DI/sfg-di/src/main/java/myspring/notes/sfgdi/controllers/MyController.java
+++ Spring-2- DI/sfg-di/src/main/java/myspring/notes/sfgdi/controllers/MyController.java
@@ -1,13 +1,19 @@
 package myspring.notes.sfgdi.controllers;
 
+import myspring.notes.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 // telling to spring controller isterio type
 @Controller
 public class MyController {
 
-    public String sayHello(){
-        System.out.println("Hello World");
+    private final GreetingService greetingService;
 
-        return "Hi Adarsh";
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        //System.out.println("Hello World");
+        return greetingService.sayGreeting();
     }
 }
